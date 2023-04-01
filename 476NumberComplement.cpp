245a15ca9e65ca47f0cd47e6b6e7f5bffe9931de
raw-h <rawat.shubham2400@gmail.com>
2023-04-01 22:48:56 +0530

    476

---
+++ 476NumberComplement.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int bitwiseCompliment(int n)
+{
+    int mask = 0;
+    int temp = n;
+    if (n == 0)
+    {
+        return 1;
+    }
+    while (temp != 0)
+    {
+        temp = temp >> 1;
+        mask = mask << 1;
+        mask = mask | 1;
+    }
+    return ((~n) & mask);
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    int answer = bitwiseCompliment(x);
+    cout << answer << endl;
+    return 0;
+}
