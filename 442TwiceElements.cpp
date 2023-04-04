6fc8c4483243b9b888d137bf637a565d04de6681
raw-h <rawat.shubham2400@gmail.com>
2023-04-04 06:10:51 +0530

    442

---
+++ 442TwiceElements.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findDuplicates(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    map<int, int> freq_map;
+    for(auto it: nums){
+        freq_map[it]++;
+    }
+    vector<int> answer;
+    for(auto it: freq_map){
+        if(it.second == 2)
+            answer.push_back(it.first);
+    }
+    return answer;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    vector<int> final_vec =  findDuplicates(arr);
+    for(auto it: final_vec){
+        cout << it << " ";
+    }
+    cout << endl;
+    return 0;
+}
