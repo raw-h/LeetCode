a9ab314d65d9ec49b817f0c1589fb0fbf4381c92
raw-h <rawat.shubham2400@gmail.com>
2023-05-12 10:43:02 +0530

    1910

--- 1910RemoveAllOccOfSubstr.cpp
+++ 1910RemoveAllOccOfSubstr.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string removeOccurances(string s, string p){
-    while((s.find(p) > 0) && (s.find(p) < s.end() - 1)){
-        s.erase(s.find(p), p.length());
+string removeOccurrences(string s, string part)
+{
+    while (s.length() != 0 && s.find(part) < s.length())
+    {
+        s.erase(s.find(part), part.length());
     }
     return s;
 }
@@ -12,7 +14,7 @@ int main()
 {
     string s, p;
     cin >> s >> p;
-    string remaining = removeOccurances(s,p);
+    string remaining = removeOccurrences(s,p);
     cout << remaining << endl;
     return 0;
 }
